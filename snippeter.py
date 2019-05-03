f418db5d427d6664d2b78abc63ba28b83c76599e
Lewis Carpenter <lewisbcarpenter_@outlook.com>
2019-05-03 14:36:34 +0100

    made pyperclip optional for snippeter

--- snippeter.py
+++ snippeter.py
@@ -1,9 +1,17 @@
-import aoeai, pyperclip
+import aoeai
 
 print("aoe2ai snippeter")
-print("Output will be copied to the clipboard.\n")
+
+copy = True
+try:
+    import pyperclip
+    print("Output will be copied to the clipboard.\n")
+except ImportError:
+    print("pyperclip not found, automatic copying disabled.")
+    copy = False
 
 while True:
     output = aoeai.translate(input(">"))
     print("\n" + output + "\n")
-    pyperclip.copy(output)
+    if copy:
+        pyperclip.copy(output)
