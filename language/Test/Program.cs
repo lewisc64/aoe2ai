786dec685853f6d975dacad746af5a956bfb913f
lewisc64 <lewisbcarpenter_@outlook.com>
2021-08-04 22:04:45 +0100

    if blocks

--- language/Test/Program.cs
+++ language/Test/Program.cs
@@ -11,36 +11,20 @@ namespace Aoe2AI
             var t = new Transpiler();
             Console.WriteLine(t.Transpile(@"
 
-#add condition current-age == feudal-age
-    #add action research ri-loom
-        rule
-    #remove action
-#remove condition
-
-#add condition current-age == feudal-age
-    #add action build town-center
-        rule
-    #remove action
-#remove condition
-
+#if a
+    rule
+#else if b
+    rule
+#else if c
+    rule
+#else
+    rule
+#end if
+rule
+#if current-age == feudal-age
+    rule
+#end if
 "));
-
-            var rule = new Defrule(
-                new Condition[] {
-                    new CombinatoryCondition("or", new[] {
-                        new Condition("current-age == dark-age"),
-                        new Condition("current-age == feudal-age"),
-                    }).Invert(),
-                    new Condition("can-build barracks"),
-                },
-                new[] {
-                    new Language.ScriptItems.Action("build barracks"),
-                });
-
-            Console.WriteLine(rule);
-            // Console.WriteLine(rule.Length);
-
-            Console.WriteLine(Condition.Parse("goal produce-scouts 1 and research-completed ri-light-cavalry and not(research-available ri-hussar) and up-research-status c: ri-hussar < research-pending"));
         }
     }
 }
