ec1287fd3e24f0d86af7b66ffeced71fbe5d2485
lewisc64 <lewisbcarpenter_@outlook.com>
2021-08-05 06:31:10 +0100

    build farms

---
+++ language/Language/Rules/BuildFarms.cs
@@ -0,0 +1,49 @@
+﻿using Language.ScriptItems;
+using System.Collections.Generic;
+
+namespace Language.Rules
+{
+    [ActiveRule]
+    public class BuildFarms : RuleBase
+    {
+        public override string Name => "build farms";
+
+        public BuildFarms()
+            : base(@"^build farms$")
+        {
+        }
+
+        public override void Parse(string line, TranspilerContext context)
+        {
+            var gathererGoal = context.CreateGoal();
+            var farmGoal = context.CreateGoal();
+
+            var infoRule = new Defrule(
+                new[]
+                {
+                    "true",
+                },
+                new[]
+                {
+                    $"up-get-fact unit-type-count villager {gathererGoal}",
+                    $"p-modify-goal {gathererGoal} s:* sn-food-gatherer-percentage",
+                    $"up-modify-goal {gathererGoal} c:/ 100",
+                    $"up-get-fact building-type-count-total farm {farmGoal}",
+                });
+
+            var buildRule = new Defrule(
+                new[]
+                {
+                    $"up-compare-goal {farmGoal} g:< {gathererGoal}",
+                    "can-build farm",
+                },
+                new[]
+                {
+                    "build-farm"
+                });
+
+            context.AddToScript(context.ApplyStacks(infoRule));
+            context.AddToScript(context.ApplyStacks(buildRule));
+        }
+    }
+}
