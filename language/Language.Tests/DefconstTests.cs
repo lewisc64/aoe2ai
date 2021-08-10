a6ce1a71e147486b3a5a487e0608b6a875c65d41
lewisc64 <lewisbcarpenter_@outlook.com>
2021-08-10 21:23:57 +0100

    defconst tostring unit test

---
+++ language/Language.Tests/DefconstTests.cs
@@ -0,0 +1,16 @@
+﻿using Language.ScriptItems;
+using System.Linq;
+using Xunit;
+
+namespace Language.Tests
+{
+    public class DefconstTests
+    {
+        [Fact]
+        public void ToString_Success()
+        {
+            var defconst = new Defconst("name", "5");
+            Assert.Equal("(defconst name 5)", defconst.ToString());
+        }
+    }
+}
