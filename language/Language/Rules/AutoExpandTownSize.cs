88fe4123f27b7d0999ff6e52bdbee7612a3cab07
lewisc64 <lewisbcarpenter_@outlook.com>
2022-01-09 13:22:52 +0000

    auto expand town size: shrink town over time

--- language/Language/Rules/AutoExpandTownSize.cs
+++ language/Language/Rules/AutoExpandTownSize.cs
@@ -7,11 +7,13 @@ namespace Language.Rules
     [ActiveRule]
     public class AutoExpandTownSize : RuleBase
     {
-        private const int MinimumTownSize = 5;
+        private const int MinimumTownSize = 8;
 
         private const int Increment = 5;
 
         private const int DefaultMaximum = 30;
+
+        private const int ShrinkInterval = 60;
 
         private static readonly IEnumerable<string> Buildings = new[]
         {
@@ -39,7 +41,7 @@ namespace Language.Rules
         public override string Name => "auto expand town size";
 
         public override string Help => @"Automatically expands the town size based on if a building is meant to be built but there are no villagers to build it.
-Can be used in conjunction with town size attacks modifying only sn-maximum-town-size. Upon activation it will shrink the town size to a minimum once and expand from there.
+Can be used in conjunction with town size attacks modifying only sn-maximum-town-size above the maximum supplied. Upon activation it will shrink the town size to a minimum once and expand from there.
 
 Affects the following sn's:
  - sn-maximum-town-size
@@ -47,10 +49,11 @@ Affects the following sn's:
  - sn-safe-town-size
  - sn-maximum-food-drop-distance";
 
-        public override string Usage => "auto expand town size";
+        public override string Usage => @"auto expand town size
+auto expand town size to 30";
 
         public AutoExpandTownSize()
-            : base(@"^auto expand town size(?: to (?<maximum>[^ ]+))$")
+            : base(@"^auto expand town size(?: to (?<maximum>[^ ]+))?$")
         {
         }
 
@@ -59,6 +62,8 @@ Affects the following sn's:
             var maximum = GetData(line)["maximum"].Value.ReplaceIfNullOrEmpty(DefaultMaximum.ToString());
 
             var buildingInQueueGoal = context.CreateGoal();
+
+            var shrinkTimer = context.CreateTimer();
 
             var rules = new List<Defrule>()
             {
@@ -72,6 +77,7 @@ Affects the following sn's:
                         $"set-strategic-number sn-maximum-town-size {MinimumTownSize}",
                         $"set-strategic-number sn-safe-town-size {MinimumTownSize}",
                         $"set-strategic-number sn-maximum-food-drop-distance {MinimumTownSize}",
+                        $"enable-timer {shrinkTimer} {ShrinkInterval}",
                         "disable-self"
                     }),
                 new Defrule(new[] { "true" }, new[] { $"set-goal {buildingInQueueGoal} 0" }),
@@ -94,17 +100,35 @@ Affects the following sn's:
             rules.Add(new Defrule(
                 new[]
                 {
+                    $"goal {buildingInQueueGoal} 0",
+                    $"strategic-number sn-maximum-town-size < {maximum}",
+                    $"strategic-number sn-minimum-town-size > {MinimumTownSize}",
+                    $"timer-triggered {shrinkTimer}",
+                },
+                new[]
+                {
+                    "up-modify-sn sn-minimum-town-size c:- 1",
+                    "up-modify-sn sn-maximum-town-size s:= sn-minimum-town-size",
+                    "up-modify-sn sn-safe-town-size s:= sn-minimum-town-size",
+                    "up-modify-sn sn-maximum-food-drop-distance s:= sn-minimum-town-size",
+                    $"disable-timer {shrinkTimer}",
+                    $"enable-timer {shrinkTimer} {ShrinkInterval}",
+                }));
+
+            rules.Add(new Defrule(
+                new[]
+                {
                     $"goal {buildingInQueueGoal} 1",
                     "unit-type-count 118 == 0",
                     "unit-type-count 212 == 0",
                     "civilian-population >= 2",
                     $"strategic-number sn-maximum-town-size < {maximum}",
-                    "not (town-under-attack)",
                 },
                 new[]
                 {
                     $"up-modify-sn sn-minimum-town-size c:+ {Increment}",
-                    $"up-modify-sn sn-maximum-town-size c:+ {Increment}",
+                    $"up-modify-sn sn-minimum-town-size c:min {maximum}",
+                    "up-modify-sn sn-maximum-town-size s:= sn-minimum-town-size",
                     "up-modify-sn sn-safe-town-size s:= sn-minimum-town-size",
                     "up-modify-sn sn-maximum-food-drop-distance s:= sn-minimum-town-size",
                 }));
