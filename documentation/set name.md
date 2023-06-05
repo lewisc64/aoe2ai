9b0438be4c2366562b8665d14867b4e28444dfef
lewisc64 <lewisbcarpenter_@outlook.com>
2023-06-05 18:55:41 +0100

    set name

---
+++ documentation/set name.md
@@ -0,0 +1,19 @@
+# set name
+Sets the name of the AI.
+## Usage
+```
+set name to "NAME"
+```
+## Examples
+```
+set name to "Cool AI"
+```
+```
+(defrule
+    (true)
+=>
+    (up-change-name "Cool AI")
+)
+
+```
+---
---
+++ language/Language/Rules/SetName.cs
@@ -0,0 +1,34 @@
+﻿using Language.ScriptItems;
+using System.Collections.Generic;
+
+namespace Language.Rules
+{
+    [ActiveRule]
+    public class SetName : RuleBase
+    {
+        public override string Name => "set name";
+
+        public override string Help => "Sets the name of the AI.";
+
+        public override string Usage => "set name to \"NAME\"";
+
+        public override IEnumerable<string> Examples => new[]
+        {
+            "set name to \"Cool AI\"",
+        };
+
+        public SetName()
+            : base(@"^set name to ""(?<name>.+)""$")
+        {
+        }
+
+        public override void Parse(string line, TranspilerContext context)
+        {
+            var data = GetData(line);
+            var name = data["name"].Value;
+
+            var rule = new Defrule(new[] { "true" }, new[] { $"up-change-name \"{name}\"" });
+            context.AddToScript(context.ApplyStacks(rule));
+        }
+    }
+}
