e2f60e3b870f1c97e941fc8249f128441351fe64
lewisc64 <lewisbcarpenter_@outlook.com>
2024-01-14 07:07:55 +0000

    basic targeting ignores 0 pop players

--- documentation/do basic targeting.md
+++ documentation/do basic targeting.md
@@ -6,20 +6,45 @@ do basic targeting
 ```
 ```
 (defrule
+    (true)
+=>
+    (set-goal 2 0)
+)
+(defrule
     (or
       (strategic-number sn-target-player-number <= 0)
       (or
         (not
           (player-in-game target-player)
         )
-        (not
-          (stance-toward target-player enemy)
+        (or
+          (not
+            (stance-toward target-player enemy)
+          )
+          (players-population target-player == 0)
         )
       )
     )
 =>
     (up-find-player enemy find-closest 1)
     (up-modify-sn sn-target-player-number g:= 1)
+    (set-goal 2 1)
+)
+(defrule
+    (goal 2 1)
+    (players-military-population target-player == 0)
+    (players-population target-player < 20)
+=>
+    (up-find-next-player enemy find-closest 1)
+)
+(defrule
+    (goal 2 1)
+    (players-military-population target-player == 0)
+    (players-population target-player < 20)
+    (up-compare-goal 1 c:>= 1)
+=>
+    (up-modify-sn sn-target-player-number g:= 1)
+    (up-jump-rule -2)
 )
 
 ```
--- language/Language/Rules/DoBasicTargeting.cs
+++ language/Language/Rules/DoBasicTargeting.cs
@@ -23,31 +23,69 @@ namespace Language.Rules
 
         public override void Parse(string line, TranspilerContext context)
         {
-            context.UsingVolatileGoal(closestPlayerGoal =>
-            {
-                var rules = new[]
-                {
-                    new Defrule(
-                        new[]
-                        {
-                            Condition.JoinConditions(
-                                "or",
-                                new[]
-                                {
-                                    new Condition("strategic-number sn-target-player-number <= 0"),
-                                    new Condition("player-in-game target-player").Invert(),
-                                    new Condition("stance-toward target-player enemy").Invert(),
-                                }),
-                        },
-                        new[]
-                        {
-                            new Action($"up-find-player enemy find-closest {closestPlayerGoal}"),
-                            new Action($"up-modify-sn sn-target-player-number g:= {closestPlayerGoal}"),
-                        }),
-                };
+            var closestPlayerGoal = context.CreateVolatileGoal();
+            var justDidRetargetGoal = context.CreateVolatileGoal();
 
-                context.AddToScript(context.ApplyStacks(rules));
-            });
+            var rules = new[]
+            {
+                new Defrule(new[] { "true" }, new[] { $"set-goal {justDidRetargetGoal} 0" }),
+                new Defrule(
+                    new[]
+                    {
+                        Condition.JoinConditions(
+                            "or",
+                            new[]
+                            {
+                                new Condition("strategic-number sn-target-player-number <= 0"),
+                                new Condition("player-in-game target-player").Invert(),
+                                new Condition("stance-toward target-player enemy").Invert(),
+                                new Condition("players-population target-player == 0"),
+                            }),
+                    },
+                    new[]
+                    {
+                        new Action($"up-find-player enemy find-closest {closestPlayerGoal}"),
+                        new Action($"up-modify-sn sn-target-player-number g:= {closestPlayerGoal}"),
+                        new Action($"set-goal {justDidRetargetGoal} 1"),
+                    }),
+                new Defrule(
+                    new[]
+                    {
+                        $"goal {justDidRetargetGoal} 1",
+                        "players-military-population target-player == 0",
+                        "players-population target-player < 20",
+                    },
+                    new[]
+                    {
+                        $"up-find-next-player enemy find-closest {closestPlayerGoal}",
+                    })
+                {
+                    Compressable = false,
+                    Splittable = false,
+                },
+                new Defrule(
+                    new[]
+                    {
+                        $"goal {justDidRetargetGoal} 1",
+                        "players-military-population target-player == 0",
+                        "players-population target-player < 20",
+                        $"up-compare-goal {closestPlayerGoal} c:>= 1",
+                    },
+                    new[]
+                    {
+                        $"up-modify-sn sn-target-player-number g:= {closestPlayerGoal}",
+                        "up-jump-rule -2",
+                    })
+                {
+                    Compressable = false,
+                    Splittable = false,
+                },
+            };
+
+            context.FreeVolatileGoal(closestPlayerGoal);
+            context.FreeVolatileGoal(justDidRetargetGoal);
+
+            context.AddToScript(context.ApplyStacks(rules));
         }
     }
 }
