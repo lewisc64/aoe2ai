78b3941c2812b7b05cbd4dee6697e093c8f01f48
lewisc64 <lewisbcarpenter_@outlook.com>
2025-03-28 00:08:03 +0000

    delay real time

---
+++ documentation/delay.md
@@ -0,0 +1,79 @@
+# delay
+Block body is only allowed to trigger after the time is up.
+## Usage
+```
+#delay by AMOUNT TIME_UNIT
+    RULES
+#end delay
+```
+## Examples
+```
+#delay by 30 seconds
+    chat to all "30 in-game seconds has passed."
+#end delay
+```
+```
+(defconst chat-b640597567a441556749250ce228bf3ad5f03a3f "30 in-game seconds has passed.")
+(defrule
+    (true)
+=>
+    (enable-timer 1 30)
+    (disable-self)
+)
+(defrule
+    (timer-triggered 1)
+=>
+    (chat-to-all chat-b640597567a441556749250ce228bf3ad5f03a3f)
+)
+
+```
+---
+```
+#delay by 30 minutes
+    chat to all "30 in-game minutes has passed."
+#end delay
+```
+```
+(defconst chat-a4b9a95b5ede9f308f8196a9b2b99623c578d34a "30 in-game minutes has passed.")
+(defrule
+    (true)
+=>
+    (enable-timer 1 1800)
+    (disable-self)
+)
+(defrule
+    (timer-triggered 1)
+=>
+    (chat-to-all chat-a4b9a95b5ede9f308f8196a9b2b99623c578d34a)
+)
+
+```
+---
+```
+#delay by 5 real seconds
+    chat to all "5 real seconds have passed."
+#end delay
+```
+```
+(defconst chat-4d67ce39e29a395c4e17c819d2bb18d357d0e065 "5 real seconds have passed.")
+(defrule
+    (true)
+=>
+    (up-get-precise-time 0 1)
+    (set-goal 2 0)
+    (disable-self)
+)
+(defrule
+    (up-compare-goal 2 c:< 5000)
+=>
+    (up-get-precise-time 0 2)
+    (up-modify-goal 2 g:- 1)
+)
+(defrule
+    (up-compare-goal 2 c:>= 5000)
+=>
+    (chat-to-all chat-4d67ce39e29a395c4e17c819d2bb18d357d0e065)
+)
+
+```
+---
--- language/Language/Rules/Delay.cs
+++ language/Language/Rules/Delay.cs
@@ -6,7 +6,7 @@ namespace Language.Rules
     [ActiveRule]
     public class Delay : RuleBase
     {
-        public override string Name => "do once";
+        public override string Name => "delay";
 
         public override string Help => "Block body is only allowed to trigger after the time is up.";
 
@@ -17,15 +17,18 @@ namespace Language.Rules
         public override IEnumerable<string> Examples => new[]
         {
             @"#delay by 30 seconds
-    chat to all ""30 seconds has passed.""
+    chat to all ""30 in-game seconds has passed.""
 #end delay",
             @"#delay by 30 minutes
-    chat to all ""30 minutes has passed.""
+    chat to all ""30 in-game minutes has passed.""
+#end delay",
+            @"#delay by 5 real seconds
+    chat to all ""5 real seconds have passed.""
 #end delay",
         };
 
         public Delay()
-            : base(@"^(?:#delay by (?<amount>[^ ]+) (?<unit>seconds?|minutes?|hours?)|#end delay)$")
+            : base(@"^(?:#delay by (?<amount>[^ ]+) (?<real>real )?(?<unit>seconds?|minutes?|hours?)|#end delay)$")
         {
         }
 
@@ -35,6 +38,7 @@ namespace Language.Rules
             {
                 var data = GetData(line);
                 var amount = int.Parse(data["amount"].Value);
+                var real = data["real"].Success;
                 var unit = data["unit"].Value.TrimEnd('s');
 
                 switch (unit)
@@ -43,11 +47,40 @@ namespace Language.Rules
                     case "hour": amount *= 3600; break;
                 }
 
-                var timerNumber = context.CreateTimer();
+                if (real)
+                {
+                    var startStampGoal = context.CreateGoal();
+                    var elapsedGoal = context.CreateGoal();
 
-                context.AddToScript(context.ApplyStacks(new Defrule(new[] { "true" }, new[] { $"enable-timer {timerNumber} {amount}", "disable-self" })));
-
-                context.ConditionStack.Push(new Condition($"timer-triggered {timerNumber}"));
+                    var rules = new[]
+                    {
+                        new Defrule(
+                            [
+                                "true"
+                            ],
+                            [
+                                $"up-get-precise-time 0 {startStampGoal}",
+                                $"set-goal {elapsedGoal} 0",
+                                "disable-self",
+                            ]),
+                        new Defrule(
+                            [
+                                $"up-compare-goal {elapsedGoal} c:< {amount * 1000}"
+                            ],
+                            [
+                                $"up-get-precise-time 0 {elapsedGoal}",
+                                $"up-modify-goal {elapsedGoal} g:- {startStampGoal}",
+                            ])
+                    };
+                    context.AddToScript(context.ApplyStacks(rules));
+                    context.ConditionStack.Push(new Condition($"up-compare-goal {elapsedGoal} c:>= {amount * 1000}"));
+                }
+                else
+                {
+                    var timerNumber = context.CreateTimer();
+                    context.AddToScript(context.ApplyStacks(new Defrule(["true"], [$"enable-timer {timerNumber} {amount}", "disable-self"])));
+                    context.ConditionStack.Push(new Condition($"timer-triggered {timerNumber}"));
+                }
             }
             else
             {
